5139e3318a5f044371c58914632e04a64630bca0
Kopal Mehta <kopalmehta4@gmail.com>
2019-12-03 21:59:29 +0530

    adding 7kyu folder with first problem

---
+++ 7-kyu/Remove anchor from URL/index.js
@@ -0,0 +1,15 @@
+/* 
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+Examples
+
+// returns 'www.codewars.com'
+removeUrlAnchor('www.codewars.com#about')
+
+// returns 'www.codewars.com?page=1' 
+removeUrlAnchor('www.codewars.com?page=1') 
+*/
+
+function removeUrlAnchor(url){
+  var index = url.indexOf('#')
+  return url.split('',index).join('');
+}
