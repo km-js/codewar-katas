d7fe6ae96308305958d8c130dfc033915668e0de
Kopal Mehta <kopalmehta4@gmail.com>
2020-02-12 22:52:05 +0530

    added 7kyu help bob count letters and digits

---
+++ 7-kyu/Help Bob count letters and digits/index.js
@@ -0,0 +1,20 @@
+/*
+
+Bob is a lazy man.
+
+He needs you to create a method that can determine how many letters and digits are in a given string.
+
+Example:
+
+"hel2!lo" --> 6
+
+"wicked .. !" --> 6
+
+"!?..A" --> 1
+
+ */
+
+function countLettersAndDigits(input) {
+	const result = input.match(/[\dA-Z]/gi);
+	return result === null ? "0" : result.length;
+}
